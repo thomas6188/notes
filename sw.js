0f44db3b9a6286c54c8de0b74a5c5b49608c9dd3
Thomas Mathew <thomas618*@gmail.com>
2020-04-19 17:58:36 +1000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b52a68b50f87db249a93.js"
+    "url": "webpack-runtime-a0b12ef96971fb25516e.js"
   },
   {
     "url": "styles.c9b5752f82f472fc39a8.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "af9663dbbd6d1a6cf6a47801cd0d862a"
+    "revision": "c645eb46774a605a6702d7fd11007a50"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-45876cc2d4c10fe60a41.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f5230c32184b9f7395c5989ba6dc19cf"
+    "revision": "b3534e4c604691913dab8a9699149194"
   },
   {
     "url": "manifest.webmanifest",
